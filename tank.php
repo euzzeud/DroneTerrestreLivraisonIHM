62f4d11c847e3815197fe6ec0a218380d682a7bd
Euzzeud <57187626+euzzeud@users.noreply.github.com>
2023-05-15 09:24:28 +0200

    ➕ Add php code for tank mode

--- tank.php
+++ tank.php
@@ -42,62 +42,128 @@
                 <form action="./tank.php" method="post">
                     <input type="hidden" name="plus_motor_left" value="10"/>
                     <input type="button" value="➕" class="plus_button button">
+                    <input type="hidden" name="leftMotor" value="<?php print $leftMotor?>"/>
+			        <input type="hidden" name="rightMotor" value="<?php print $rightMotor?>"/>
+			        <input type="hidden" name="counter" value="<?php print $counter?>"/>
                 </form>
                 <form action="./tank.php" method="post">
                     <input type="hidden" name="stop_motor_left" value="0"/>
                     <input type="button" value="stop" class="stop_button button">
+                    <input type="hidden" name="leftMotor" value="<?php print $leftMotor?>"/>
+			        <input type="hidden" name="rightMotor" value="<?php print $rightMotor?>"/>
+			        <input type="hidden" name="counter" value="<?php print $counter?>"/>
                 </form>
                 <form action="./tank.php" method="post">
                     <input type="hidden" name="substract_motor_left" value="-10"/>
                     <input type="button" value="➖" class="substract_button button">
+                    <input type="hidden" name="leftMotor" value="<?php print $leftMotor?>"/>
+			        <input type="hidden" name="rightMotor" value="<?php print $rightMotor?>"/>
+			        <input type="hidden" name="counter" value="<?php print $counter?>"/>
                 </form>
             </div>
             <div id="buttons_right">
                 <form action="./tank.php" method="post">
                     <input type="hidden" name="plus_motor_right" value="10"/>
                     <input type="button" value="➕" class="plus_button button">
+                    <input type="hidden" name="leftMotor" value="<?php print $leftMotor?>"/>
+			        <input type="hidden" name="rightMotor" value="<?php print $rightMotor?>"/>
+			        <input type="hidden" name="counter" value="<?php print $counter?>"/>
                 </form>
                 <form action="./tank.php" method="post">
                     <input type="hidden" name="stop_motor_right" value="0"/>
                     <input type="button" value="stop" class="stop_button button">
+                    <input type="hidden" name="leftMotor" value="<?php print $leftMotor?>"/>
+			        <input type="hidden" name="rightMotor" value="<?php print $rightMotor?>"/>
+			        <input type="hidden" name="counter" value="<?php print $counter?>"/>
                 </form>
                 <form action="./tank.php" method="post">
                     <input type="hidden" name="substract_motor_right" value="-10"/>
                     <input type="button" value="➖" class="substract_button button">
+                    <input type="hidden" name="leftMotor" value="<?php print $leftMotor?>"/>
+			        <input type="hidden" name="rightMotor" value="<?php print $rightMotor?>"/>
+			        <input type="hidden" name="counter" value="<?php print $counter?>"/>
                 </form>
                 
-                <?php
-		$fichier="motorsStates.txt";
-		if (file_exists($fichier)) {
-			echo "Le fichier ".$fichier." existe.";
-		} else {
-			echo "Fichier introuvable...";
-			$moteur=0;
-			touch($fichier);
-			if ($fp=fopen($fichier,"a")) {
-				fwrite($fp,"moteur à 0\n");
-			} else {
-				echo "<br />Impossible d'ouvrir le fichier ".$fichier.".";
-			}
-		}
+        <?php
+		$file="motorsStates.txt";
+		if (file_exists($file)) {
+			echo "Le fichier ".$file." existe.";
+        } else {
+            echo "Aucun fichier trouvé.";
+            $rightMotor="0";
+            $leftMotor="0";
+            touch($file);
+            if ($fp=fopen($fichier, "w")) {
+                fwrite($fp, ("0 0 0\n"));
+                fclose($fp);
+            } else {
+                echo "Impossible d'ouvrir le fichier ".$file.".";
+            }
+            }
         
 		if ($_SERVER["REQUEST_METHOD"]=="POST") {
-			echo "<br />"."il s'agit bien d'une méthode POST.";
-			echo $_POST["plus_motor_left"];
-			echo "  ";
-			echo $_POST["stop_motor_left"];
-			echo "  ";
-			echo $_POST["substract_motor_left"];
-            echo $_POST["plus_motor_right"];
-			echo "  ";
-			echo $_POST["stop_motor_right"];
-			echo "  ";
-			echo $_POST["substract_motor_right"];
+			$leftMotor=$_POST["left_motor"];
+            $rightMotor=$_POST["right_motor"];
+            $counter=$_POST["counter"];
+
+            /* LEFT MOTOR */
+
+            $x=intval($leftMotor);
+            if ($_POST["plus_motor_left"]!="");
+            if (x<100) {
+                $x += $y;
+            }
+
+            } else if ($_POST["stop_motor_left"]!="") {
+                $x = 0;
+
+            } else if ($_POST["substract_motor_left"]!="") {
+                $y=intval($_POST["substract_motor_left"]);
+                if ($x>-100) {
+                    $x += $y;
+                }
+            }
+
+            $leftMotor=strval($x);
+
+            /* RIGHT MOTOR */
+
+            $x=intval($rightMotor);
+            if ($_POST["plus_motor_right"]!="");
+            if (x<100) {
+                $x += $y;
+                
+            } else if ($_POST["stop_motor_right"]!="") {
+                $x = 0;
+                
+            } else if ($_POST["substract_motor_right"]!="") {
+                $y=intval($_POST["substract_motor_right"]);
+                if ($x>-100) {
+                    $x += $y;
+                }
+            }
+
+            $rightMotor=strval($x);
+
+            /* COUNTER */
+
+            $x=intval($counter);
+            $x+=1;
+            $counter=strval(x);
+            if ($fp=fopen($file, "w")) {
+                fwrite($fp, $counter." ".$leftMotor." ".$rightMotor&."\n");
+				fclose($fp);
+            } else {
+                echo "Impossible d'ouvrir le fichier ".$file.".";
+            }
+
+
 		}
 		?>
         </div>
         </div>
-    <div id="coordinates_container">
+
+        <div id="coordinates_container">
         <h4 class="container_title">Coordonnées de départ</h4>
         <form action="">
             <label for="latitude">Latitude :</label>
@@ -111,5 +177,16 @@
             <input type="button" value="Valider" id="submit_button">
         </form>
     </div>
+
+        <?php echo "<br>" .htmlspecialchars($_SERVER["PHP_SELF"])."<br>";?>
+        <?php echo $_SERVER["REQUEST_METHOD"]."<br>";?>
+
+        <?php
+		if ($_SERVER["REQUEST_METHOD"]=="POST") {
+			echo "<br>"."leftMotor = ".$leftMotor;
+			echo " "."rightMoteur = ".$rightMotor;
+			echo "<br>"."counter = ".$counter;
+		}
+		?>
 </body>
 </html>
